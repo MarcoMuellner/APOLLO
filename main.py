fc0fba5566440c46d900564869dee48e93d26c6a
Marco Müllner <Marco@marcos-mbp.home>
2017-04-18 16:23:55 +0200

    - Made first call attempt of filehandlers

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
-#Library Includes
-from PyQt5.QtWidgets import QApplication
-import sys
-#
-#a = QApplication(sys.argv)
-
-#import qt5reactor
-#try:
-#    qt5reactor.install()
-#except:
-#    print("Allready Installed")
-
-#from twisted.internet import reactor
-#
-#Application Includes
-#from gui.gui import MainWindow
 from loghandler import loghandler
-from runner import StandardRunner
+from filehandler.Diamonds.diamondsResultsFile import Results
 
 
 loghandler.setup_logging()
 
-run = StandardRunner()
+result = Results('002436458','00')
 
 
-#Use this when you want to start GUI stuff!
-#w = MainWindow(reactor)
-#w.show()
-#reactor.run(
