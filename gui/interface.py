de7f28f72f8cf828696e1e62a1d46f15f1c65f30
mm7490 <muellnermarco@gmail.com>
2017-08-23 13:36:38 +0200

    Added a basic gui implementation of the future RunWindow

--- gui/interface.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QWidget
-
-class MainWindow:
-    def __init__(self):
-        self.__createWindow()
-    def go(self):
-        self.mainWidget.show()
-    def __createWindow(self):
-        self.mainWidget = QWidget()
