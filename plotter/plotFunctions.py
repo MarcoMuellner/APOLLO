abf466c1877952d9a784e2f0cc1258db6806eaae
mm7490 <muellnermarco@gmail.com>
2018-02-07 12:11:20 +0100

    Removed unnecessary plotfunctions (old or deprecated)

--- plotter/plotFunctions.py
+++ plotter/plotFunctions.py
@@ -136,38 +136,6 @@ def plotPSD(data, psdOnly, markerList=None, smooth=True, visibilityLevel=0, file
         saveFigToResults(data.kicID, fileName, p)
 
 
-def plotMarginalDistributions(data,visibilityLevel=0,fileName=0):
-    '''
-        This method provides the possibility to plot the marginal Distributions of the DIAMONDS run. Using the raw data
-    from the summaryFileModel, and the MarginalDistribution of the data it draws the plot. The size of the plot
-    is determined by the count of the parameters.Due to the inconsistency and different plotting devices, show() needs
-    to be called.
-    :param data: The data which will be plotted. Has to have a getMarginalDistribution() method and a summary property
-    assigned to it
-    :param visibilityLevel:This parameter defines if the plot is shown or not. If visibilityLevel<SettingsLevel it will
-    be shown
-    :type visibilityLevel:int
-    :param fileName:The filename with which you want to save the plot. If it is empty, the plot will not be saved
-    :type fileName:str
-    '''
-    pl.figure(figsize=(23, 12))
-    marginalDists = data.getMarginalDistribution()
-    summary = data.summary
-
-    par_median = summary.getRawData(strSummaryMedian)  # median values
-
-    for iii in range(0, len(marginalDists)):
-        pl.subplot(2, 5, iii + 1)
-        par, marg, fill_x, fill_y, par_err = marginalDists[iii].getMarginalDistribution()
-
-        pl.xlim([par_median[iii] - 5 * par_err, par_median[iii] + 5 * par_err])
-        pl.ylim([0, max(marg) * 1.2])
-        pl.plot(par, marg, linewidth=2, c='k')
-        pl.fill_between(fill_x, fill_y, 0, alpha=0.5, facecolor='green')
-        pl.axvline(par_median[iii], c='r')
-        pl.xlabel(marginalDists[iii].name + ' (' + marginalDists[iii].unit + ')', fontsize=16)
-
-
 def plotParameterTrend(data,fileName):
     """
     Plots the parametertrend of the data. Size is automatically determined. Due to the inconsistency and different
@@ -187,100 +155,6 @@ def plotParameterTrend(data,fileName):
 
     if fileName != "":
         saveFigToResults(data.kicID, fileName, fig)
-    return fig
-
-
-def plotDeltaNuFit(deltaNuCalculator, kicID, visibilityLevel=0):
-    """
-    Legacy
-    """
-    debugLevel = int(Settings.Instance().getSetting(strMiscSettings, strSectDevMode).value)
-    deltaNuEst = deltaNuCalculator.deltaNuEst
-    deltaF = deltaNuCalculator.deltaF
-    best_fit = deltaNuCalculator.bestFit
-    corrs = deltaNuCalculator.correlations
-
-    dfData = pd.DataFrame({r'Frequency [$\mu$Hz]': deltaF,
-                           'Correlation': corrs,
-                           'Best fit': deltaNuCalculator.gaussian(deltaF, *best_fit)
-                           })
-
-    p = ggplot(dfData, aes(x=r'Frequency [$\mu$Hz]'))
-    p = p + geom_line(aes(y=r'Correlation'), color='black',
-                      linetype='solid', label='Correlated Data')
-    p = p + geom_line(aes(y='Best fit'), color='red', linetype='dotted', label='Best Fit')
-    p = p + ggtitle(r'Autocorrelation and $\Delta$$\nu$ Fit for KIC' + kicID)
-    p = p + ylab('Autocorrelation')
-    p = p + xlab(r'Frequency [$\mu$Hz]')
-    p = p + ylim(0, 1.5 * max(deltaNuCalculator.gaussian(deltaF, *best_fit)))
-    p = p + xlim(deltaNuEst - 0.2 * deltaNuEst, deltaNuEst + 0.2 * deltaNuEst)
-    p = p + geom_vline(x=[deltaNuEst], linetype='dashed', color='blue')
-    if visibilityLevel <= debugLevel:
-        print(p)
-    return p
-
-
-def plotStellarRelations(kicList, x, y, xError, yError, xLabel, yLabel, Title, scaley='linear', scalex='linear',
-                         fitDegree=None, fill=True):
-    """
-    Legacy
-    """
-    fig = pl.figure()
-    ax = fig.add_subplot(111)
-    legendList = []
-    datapoints = pl.errorbar(x, y,
-                             xerr=xError,
-                             yerr=yError,
-                             fmt='o',
-                             capsize=5,
-                             color='k',
-                             ecolor='k',
-                             markerfacecolor='g',
-                             label='Datapoints')
-    ax.set_yscale(scaley, nonposx='clip')
-    ax.set_xscale(scalex, nonposx='clip')
-    xlen = 1.2 * max(x) - 0.8 * min(x)
-    ylen = 1.2 * max(y) - 0.8 * min(y)
-
-    idOffset = {"002436458": (0.008, 0),
-                "008264006": (0.008, 0),
-                "008263801": (0.042, 0.057),
-                "008329894": (0.008, 0),
-                "008264074": (0.008, 0.023),
-                "008196817": (-0.19, -0.023),
-                "008366239": (-0.19, -0.037),
-                "008264079": (0.012, 0)}
-    for xyz in zip(x, y, kicList):  # <--
-        string = "KIC " + xyz[2][2:]
-        ax.annotate('%s' % string, xy=(xyz[0] + idOffset[xyz[2]][0] * xlen, xyz[1] + idOffset[xyz[2]][1] * ylen),
-                    textcoords='data', fontsize=10)
-
-    legendList.append(datapoints)
-    pl.xlim(0.8 * min(x), 1.2 * max(x))
-    pl.ylim(0.8 * min(y), 1.2 * max(y))
-
-    if fitDegree is not None:
-        yError = np.power(np.array(yError), 1)
-        popt, cov = np.polyfit(np.array(x), np.array(y), deg=fitDegree, w=yError, cov=True)
-        perr = np.sqrt(np.diag(cov))
-        chi_squared = np.sum((np.polyval(popt, x) - y) ** 2)
-        f = np.poly1d(popt)
-
-        x = np.linspace(0, 1000, 10000)
-        fit = pl.plot(x, f(x), linewidth=0.5, label='Polynomial fit of degree ' + str(fitDegree))
-        if fill:
-            pl.fill_between(x, popt[0] - perr[0], popt[0] + perr[0], color='grey', alpha=0.5)
-            pl.fill_between(x, popt[0] - 2 * perr[0], popt[0] + 2 * perr[0], color='dimgrey', alpha=0.5)
-        legendList.append(fit)
-        logger.debug(
-            "Polynomial fit degree '" + str(fitDegree) + "', parameters '" + str(popt) + "' and uncertainties '" + str(
-                perr) + "'")
-        logger.debug("Chi squared is '" + str(chi_squared) + "'")
-
-    pl.legend()
-    pl.xlabel(xLabel)
-    pl.ylabel(yLabel)
-    pl.title(Title)
     return fig
 
 
