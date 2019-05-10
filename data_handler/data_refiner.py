- Sigma clipping now applied only to 4 sigma - Computing radius, mass and log_g using scaling relations - prior ranges adapted for main sequence stars - fixed error in plotting (wrong limits for psd) - added internal id for each run - removed error messages for exceptions - simplified printer for multiprocessing

data_handler/data_refiner.py
@@ -215,7 +215,7 @@ def remove_stray(data:np.ndarray,kwargs : Dict) -> np.ndarray:
 
     list_data = []
     for i in [0,1]:
-        list_data.append(data[i][np.logical_and(data[1] > cen - 5 * wid, data[1] < cen + 5 * wid)])
+        list_data.append(data[i][np.logical_and(data[1] > cen - 4 * wid, data[1] < cen + 4 * wid)])
 
     data = np.array(list_data)
 

evaluators/compute_priors.py
@@ -67,9 +67,9 @@ def sigma(nu_max: float) -> float:
     :param nu_max: nu_max value determined by pipe
     :return: envelope of oscillation
     """
-    sigma_env = 0.66 * pow(nu_max, 0.88)  # Full Width Half maximum
+    gamma_env = 0.66 * pow(nu_max, 0.88)  # Full Width Half maximum
 
-    return sigma_env * (1 / (2 * np.sqrt(2 * np.log(2))))
+    return gamma_env * (1 / (2 * np.sqrt(2 * np.log(2))))
 
 
 def amp(nu_max: float, sigma: float, f_data: np.ndarray):
@@ -143,14 +143,14 @@ def priors(nu_max: float, data: np.ndarray, kwargs: Dict):
     else:
         return [
                    [0.5 * noise(f_data), 3 * noise(f_data)],
-                   [0.05 * harvey_amp(nu_max), 1.5*harvey_upper_prior],
+                   [0.01 * harvey_amp(nu_max), 1.5*harvey_upper_prior],
                    [lower_harvey_1, max_harvey_1],
-                   [0.05 * harvey_amp(nu_max), harvey_upper_prior],
+                   [0.01 * harvey_amp(nu_max), harvey_upper_prior],
                    [minimum_percentage_harvey_1 * second_harvey(nu_max), 1.3 * second_harvey(nu_max)],
-                   [0.05 * harvey_amp(nu_max), harvey_upper_prior],
+                   [0.01 * harvey_amp(nu_max), harvey_upper_prior],
                    [0.7 * third_harvey(nu_max), 1.4 * third_harvey(nu_max)],
                    [0.1 * amp(nu_max, sigma(nu_max), f_data), 3.5 * amp(nu_max, sigma(nu_max), f_data)],
                    # [0.75 * nu_max                               , 1.25 * nu_max],
-                   [0.9 * nu_max, 1.1 * nu_max],
-                   [0.7 * sigma(nu_max), 1.5 * sigma(nu_max)]
+                   [0.8 * nu_max, 1.2 * nu_max],
+                   [0.7 * sigma(nu_max), 2 * sigma(nu_max)]
                ], params

plotter/plot_handler.py
@@ -300,7 +300,7 @@ def plot_f_space(f_data: np.ndarray, kwargs: dict, add_smoothing: bool = False, 
     if f_list is not None:
         pl.legend()
     ax.set_xlim(min(f_data[0]), max(f_data[0]))
-    ax.set_ylim(min(f_data[1] * 0.95), max(f_data[1]) * 1.2)
+    ax.set_ylim(min(f_data[1][f_data[1] > 0] * 0.95), max(f_data[1]) * 1.2)
 
     if plot_show in kwargs.keys() and kwargs[plot_show]:
         pl.show(fig)

runner/runner.py
@@ -29,9 +29,14 @@ from res.conf_file_str import general_nr_of_cores, analysis_list_of_ids, general
     cat_general, cat_plot, internal_literature_value, analysis_folder_prefix, general_sequential_run, \
     analysis_noise_values, internal_noise_value, analysis_number_repeats, internal_run_number, internal_delta_nu, \
     internal_mag_value, internal_teff, internal_path, general_run_diamonds, internal_force_run,general_check_bayes_run,\
-    analysis_nr_noise_points,analysis_target_magnitude,analysis_nr_magnitude_points,internal_multiple_mag
+    analysis_nr_noise_points,analysis_target_magnitude,analysis_nr_magnitude_points,internal_multiple_mag,analysis_nu_max_outer_guess,internal_id
 from support.exceptions import ResultFileNotFound, InputFileNotFound, EvidenceFileNotFound
+import uuid
 
+def deepcopy_dict(dict_object : Dict):
+    cp = deepcopy(dict_object)
+    cp[internal_id] = str(uuid.uuid4())
+    return cp
 
 def add_value_to_kwargs(kwargs, val, names, parameter, type_val):
     if names[0] in val.dtype.names:
@@ -144,7 +149,7 @@ def kwarg_list(conf_file: str) -> Tuple[List[Dict], int]:
 
         for i in data:
             for j in range(1, repeat):
-                cp = deepcopy(copy_dict)
+                cp = deepcopy_dict(copy_dict)
 
                 try:
                     cp = add_value_to_kwargs(cp, i, ['id'], general_kic, int)
@@ -180,7 +185,7 @@ def kwarg_list(conf_file: str) -> Tuple[List[Dict], int]:
 
                     for k in noise_values:
                         k = float('%.1f' % k)
-                        newcp = deepcopy(cp)
+                        newcp = deepcopy_dict(cp)
                         newcp[internal_noise_value] = k
                         if analysis_folder_prefix in cp:
                             pre = newcp[analysis_folder_prefix]
@@ -205,7 +210,7 @@ def kwarg_list(conf_file: str) -> Tuple[List[Dict], int]:
                     mag_values = np.linspace(min_mag,kwargs[cat_analysis][analysis_target_magnitude],nr)
                     for k in mag_values:
                         k = float('%.1f' % k)
-                        newcp = deepcopy(cp)
+                        newcp = deepcopy_dict(cp)
                         copy_dict["Original magnitude"] = i['mag']
                         newcp[internal_mag_value] = k
                         if analysis_folder_prefix in cp:
@@ -289,7 +294,10 @@ def run_star(kwargs: Dict):
 #            if internal_literature_value in kwargs:
 #                nu_max = kwargs[internal_literature_value].nominal_value
 #            else:
-            nu_max = compute_fliper_exact(data, kwargs)
+            if analysis_nu_max_outer_guess in kwargs.keys():
+                nu_max = kwargs[analysis_nu_max_outer_guess]
+            else:
+                nu_max = compute_fliper_exact(data, kwargs)
             #nu_max = kwargs[internal_literature_value].nominal_value
 
             f_fliper = nu_max